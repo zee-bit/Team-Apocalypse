c0a81122f3047644bcc285d45e515f7e7eeca518
zee-bit <equbalzeeshan@gmail.com>
2020-11-06 13:23:53 +0530

    Updated render function

--- src/render.js
+++ src/render.js
@@ -18,7 +18,7 @@ function toggleAudio() {
 }
 
 function startModel() {
-    textContainer.innerText = ''
+    textContainer.innerText = ""
 
     let dummyText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
 
@@ -30,6 +30,8 @@ function startModel() {
     })
 }
 
-function stopMode() {
-    
+// Stop Model won't work until we have a dynamic array
+// But this should work on our use case
+function stopModel() {
+
 }
